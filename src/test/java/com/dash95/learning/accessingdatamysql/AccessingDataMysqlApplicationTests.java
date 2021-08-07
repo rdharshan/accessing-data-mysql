fe89d79fb17e298fc584180aef1318bdf0b56b1f
rdharshan <darshanramakrishna@gmail.com>
2021-08-07 17:13:03 +0530

    initial commit

---
+++ src/test/java/com/dash95/learning/accessingdatamysql/AccessingDataMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dash95.learning.accessingdatamysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
